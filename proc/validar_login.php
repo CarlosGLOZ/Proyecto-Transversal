a41bc9dd39ce175c5e162f9ead9267b74eb8eba7
Mugaty7 <10000540.joan23@fje.edu>
2022-05-19 10:41:01 +0200

    10:40

--- proc/validar_login.php
+++ proc/validar_login.php
@@ -1,26 +1,35 @@
 <?php
+session_start();
 
 // comprobar que los datos han sido introducidos en el formulario, redirigir al login con un GET de validación falsa
 if ((isset($_POST['email_login']) && isset($_POST['password_login'])) && (!empty($_POST['email_login']) && !empty($_POST['password_login'])))    {
     require "./conexion.php";
 
+    // SI EL USUARIO YA ESTÁ LOGUEADO CON EL MAIL INTRODUCIDO Y HA HECHO LA VALIDACIÓN EN DOS PASOS, ENVIARLO  A index.php
+    if (isset($_SESSION['2step_val']) && isset($_SESSION['2step_val']) && $_SESSION['email_prof'] == $_POST['email_login']) {
+        echo "<script>window.location.href = '../view/index.php'</script>";
+        die();
+    }
+
     // EVITAR INJECCIÓN SQL
     $email_login = $conexion->real_escape_string($_POST['email_login']);
     $password_login = $conexion->real_escape_string($_POST['password_login']);
 
-    // ENCRIPTAR PASSWORD
-    $password_login = sha1($password_login);
+    // ENCRIPTAR PASSWORD si no es una cuenta de testeo
+    if ($email_login !== '100007082.joan23@fje.edu') {
+        $password_login = sha1($password_login);
+    }
 
 
     // COMPROBAR LA CONEXIÓN A LA BDD
     if (!$conexion) {
         echo "ERROR DE CONEXION CON LA BASE DE DATOS";
-        echo "<a href='../view/login.php'>volver</a>";
+        echo "<a href='../view/login.php?err=cnxBDD'>volver</a>";
         die();
     }
 
     // QUERY PARA VER SI EXISTE UN USUARIO CON ESOS DATOS
-    $query = "SELECT * FROM tbl_professor WHERE email_prof = '{$email_login}' AND contra = '{$password_login}'";
+    $query = "SELECT * FROM tbl_professor WHERE email_prof = '{$email_login}' AND contra = '{$password_login}';";
     $valid_login = mysqli_query($conexion, $query);
 
     $match = $valid_login -> num_rows;
@@ -43,9 +52,9 @@ if ((isset($_POST['email_login']) && isset($_POST['password_login'])) && (!empty
 
         twostep_auth($_SESSION['email_prof']);
 
-        echo "<script>window.location.href = '../view/codigo_auth.html';</script>";
         echo "<script>localStorage.setItem('nombre_usuario', '{$_SESSION['nom_prof']}' )</script>";
-        echo "<script>window.location.href = '../view/index.php';</script>";
+        echo "<script>window.location.href = '../view/codigo_auth.html';</script>";
+        die();
     } else {
         echo "<script>window.location.href = '../view/login.php?val=false';</script>";
         die();
