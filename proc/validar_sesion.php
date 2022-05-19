355be72d3b17a3f07e8749b307705eccdcc6a5ae
Mugaty7 <10000540.joan23@fje.edu>
2022-05-19 10:36:55 +0200

    10:36

--- proc/validar_sesion.php
+++ proc/validar_sesion.php
@@ -2,7 +2,7 @@
 
 function val_sesion() {
     session_start();
-    if (!isset($_SESSION['email_prof'])) {
+    if (!isset($_SESSION['email_prof']) || !$_SESSION['2step_val']) {
         echo "<script>window.location.href = '../view/login.php'</script>";
         die();
     }
